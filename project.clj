598268f190ee44564ae51358ec9f862d0588b13e
Charles Gruenwald <charlesg3@gmail.com>
2019-02-04 10:41:30 -0700

    lein-release plugin: preparing 0.10.10 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techascent/re-frame "0.10.10-SNAPSHOT"
+(defproject techascent/re-frame "0.10.10"
   :description  "A Clojurescript MVC-like Framework For Writing SPAs Using Reagent."
   :url          "https://github.com/Day8/re-frame.git"
   :license      {:name "MIT"}
