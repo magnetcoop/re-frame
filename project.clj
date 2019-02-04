8401095a340ea72fe4cd12b8ab6cc13c7217443f
Charles Gruenwald <charlesg3@gmail.com>
2019-02-04 11:42:17 -0700

    lein-release plugin: bumped version from 0.10.14 to 0.10.15-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techascent/re-frame "0.10.14"
+(defproject techascent/re-frame "0.10.15-SNAPSHOT"
   :description  "A Clojurescript MVC-like Framework For Writing SPAs Using Reagent."
   :url          "https://github.com/Day8/re-frame.git"
   :license      {:name "MIT"}
